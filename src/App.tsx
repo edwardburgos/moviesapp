ed915ffb537b6e2add9bc3a15ae09418b7a3d1cb
EdwardBurgos <edwardpbn@gmail.com>
2021-09-22 08:29:41 -0500

    app configuration and styles | searchbar component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css';
+import videocam from './img/icons/videocam-outline.svg'
+import SearchBar from './components/SearchBar/SeachBar'
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.container}>
+
+      <div className={s.titleContainer}>
+        <img src={videocam} className={s.logo} alt={'Movies app'} />
+        <h1 className='mb-0'>Movies app</h1>
+      </div>
+
+      <SearchBar></SearchBar>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/SearchBar/SeachBar.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import s from './App.module.css';
+import { Form } from 'react-bootstrap';
+import closeCircle from './img/icons/close-circle-outline.svg';
+import search from './img/icons/search-outline.svg'
+
+export default function SearchBar() {
+
+    const [name, setName] = useState<string>('')
+
+    return (
+        <div className={`${s.test} ${s.searchInput}`}>
+            <Form.Control value={name} className={s.input} placeholder="Search a movie" onChange={e => { setName(e.target.value) }} />
+            <img src={name ? closeCircle : search} className={s.iconDumb} onClick={() => setName('')} alt={name ? 'Remove movie title' : 'Search a movie'} />
+        </div>
+    );
+}
