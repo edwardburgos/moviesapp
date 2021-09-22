757c7ade9e22bcf8eb7c496528c036523612112e
EdwardBurgos <edwardpbn@gmail.com>
2021-09-22 09:33:24 -0500

    try catch statement in request | message if error

--- src/components/SearchBar/SeachBar.tsx
+++ src/components/SearchBar/SeachBar.tsx
@@ -5,8 +5,9 @@ import closeCircle from '../../img/icons/close-circle-outline.svg';
 import search from '../../img/icons/search-outline.svg';
 import axios from 'axios';
 import { SearchResult } from '../../extras/types';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { modifyMovies } from '../../actions';
+import { showMessage } from '../../extras/functions';
 
 export default function SearchBar() {
 
@@ -14,13 +15,17 @@ export default function SearchBar() {
     const [title, setTitle] = useState<string>('');
 
     async function searchMovie(title: string) {
-        const movies = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${title}&page=1`)
-        dispatch(modifyMovies(movies.data.results.map((e: SearchResult) => {return {title: e.title}})))
+        try {
+            const movies = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${title}&page=1`)
+            dispatch(modifyMovies(movies.data.results.map((e: SearchResult) => { return { title: e.title } })))
+        } catch (e) {
+            showMessage('Sorry, an error ocurred')
+        }
     }
 
     return (
         <div className={`${s.test} ${s.searchInput}`}>
-            <Form.Control value={title} className={s.input} placeholder="Search a movie" onChange={e => { setTitle(e.target.value); if (e.target.value) { searchMovie(e.target.value)}}} />
+            <Form.Control value={title} className={s.input} placeholder="Search a movie" onChange={e => { setTitle(e.target.value); if (e.target.value) { searchMovie(e.target.value) } }} />
             <img src={title ? closeCircle : search} className={s.iconDumb} onClick={() => setTitle('')} alt={title ? 'Remove movie title' : 'Search a movie'} />
         </div>
     );
