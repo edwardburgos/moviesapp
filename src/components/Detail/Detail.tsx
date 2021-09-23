aedaf4f9d18855158d34cfe341c7dd50d8fad76f
EdwardBurgos <edwardpbn@gmail.com>
2021-09-23 17:46:44 -0500

    detail component style

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,15 +1,17 @@
 import { DetailProps, MovieDetail } from '../../extras/types';
 import defaultPoster from '../../img/defaultPoster.jpg';
+import s from './Detail.module.css'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { isoLangs } from '../../extras/globalVariables';
 
 export default function Detail({ id }: DetailProps) {
 
     const [movie, setMovie] = useState<MovieDetail>({
         adult: false, belongs_to_collection: { id: 0, name: "" },
-        budget: 0, genres: [{ id: 0, name: "" }], id: 0, original_language: "", original_title: "", overview: "",
+        budget: 0, genres: [{ id: 0, name: "" }], id: 0, original_language: "en", original_title: "", overview: "",
         poster_path: "", production_companies: [{ id: 0, name: "" }], release_date: "", revenue: 0,
         status: "", title: "", vote_average: 0, vote_count: 0
     })
@@ -32,68 +34,72 @@ export default function Detail({ id }: DetailProps) {
     }, [id])
 
     return (
-        <div>
-            <h1>{movie.title}</h1>
-            {movie.title !== movie.original_title ?
-                <>
-                    <span>Original Title</span>
-                    <p>{movie.original_title}</p>
-                    <span>Original Language</span>
-                    <p>{movie.original_language}</p>
-                </> : null}
-            {movie.overview ?
-                <>
-                    <span>Overview</span>
-                    <p>{movie.overview}</p>
-                </> : null}
-            {movie.status !== 'Canceled' ?
-                <>
-                    <span>Release date</span>
-                    <p>{movie.release_date}</p>
-                </> : null}
-            <span>Suitable for</span>
-            <p>{movie.adult ? 'Adults only' : 'Whole family'}</p>
-            {movie.status !== 'Released' ?
-                <>
-                    <span>Current state</span>
-                    <p>{movie.status === 'Post Production' ? 'In ' : ''}{movie.status}</p>
-                </> : null}
-            {movie.belongs_to_collection ?
-                <>
-                    <span>Collection</span>
-                    <Link to={`/collection/${movie.belongs_to_collection.id}`}>{movie.belongs_to_collection.name}</Link>
-                </> : null}
-            {movie.budget ?
-                <>
-                    <span>Budget</span>
-                    <p>$ {movie.budget.toLocaleString()}</p>
-                </> : null}
-            {movie.revenue ?
-                <>
-                    <span>Revenue</span>
-                    <p>$ {movie.revenue.toLocaleString()}</p>
-                </> : null}
-            <span>Genres</span>
-            {movie.genres.length === 1 ?
-                <Link to={`/genre/${movie.genres[0].name.toLowerCase()}`}>{movie.genres[0].name}</Link>
-                :
-                <ul>{movie.genres.map(e => <li><Link to={`/genre/${e.name.toLowerCase()}`}>{e.name}</Link></li>)}</ul>
-            }
-            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : defaultPoster} alt={movie.title}></img>
-            <span>Production companies</span>
-            {movie.production_companies.length === 1 ?
-                <Link to={`/genre/${movie.production_companies[0].name.toLowerCase()}`}>{movie.production_companies[0].name}</Link>
-                :
-                <ul>{movie.production_companies.map(e => <li><Link to={`/genre/${e.name.toLowerCase()}`}>{e.name}</Link></li>)}</ul>}
-            {movie.vote_average && movie.vote_count ?
-                <>
-                    <span>Vote Count</span>
-                    <p>{movie.vote_count}</p>
-                    <span>Vote Average</span>
-                    <p>{movie.vote_average} / 10</p>
-                </>
-                : null
-            }
+        <div className={s.container}>
+            <div className={s.posterContainer}>
+                <img className={s.poster} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : defaultPoster} alt={movie.title}></img>
+            </div>
+            <div className={s.info}>
+                <h1 className={s.title}>{movie.title}</h1>
+                {movie.title !== movie.original_title ?
+                    <>
+                        <span className='bold'>Original Title</span>
+                        <p>{movie.original_title}</p>
+                        <span className='bold'>Original Language</span>
+                        <p>{isoLangs[movie.original_language].name}</p>
+                    </> : null}
+                {movie.overview ?
+                    <>
+                        <span className='bold'>Overview</span>
+                        <p>{movie.overview}</p>
+                    </> : null}
+                {movie.status !== 'Canceled' ?
+                    <>
+                        <span className='bold'>Release date</span>
+                        <p>{movie.release_date}</p>
+                    </> : null}
+                <span className='bold'>Suitable for</span>
+                <p>{movie.adult ? 'Adults only' : 'Whole family'}</p>
+                {movie.status !== 'Released' ?
+                    <>
+                        <span className='bold'>Current state</span>
+                        <p>{movie.status === 'Post Production' ? 'In ' : ''}{movie.status}</p>
+                    </> : null}
+                {movie.belongs_to_collection ?
+                    <>
+                        <span className='bold'>Collection</span>
+                        <p><Link className='customLink' to={`/collection/${movie.belongs_to_collection.id}/${movie.belongs_to_collection.name.toLowerCase().replaceAll(' ', '-')}`}>{movie.belongs_to_collection.name}</Link></p>
+                    </> : null}
+                {movie.budget ?
+                    <>
+                        <span className='bold'>Budget</span>
+                        <p>$ {movie.budget.toLocaleString()}</p>
+                    </> : null}
+                {movie.revenue ?
+                    <>
+                        <span className='bold'>Revenue</span>
+                        <p>$ {movie.revenue.toLocaleString()}</p>
+                    </> : null}
+                <span className='bold'>Genres</span>
+                {movie.genres.length === 1 ?
+                    <p><Link className='customLink' to={`/genre/${movie.genres[0].id}/${movie.genres[0].name.toLowerCase().replaceAll(' ', '-')}`}>{movie.genres[0].name}</Link></p>
+                    :
+                    <ul>{movie.genres.map(e => <li><Link className='customLink' to={`/genre/${e.id}/${e.name.toLowerCase().replaceAll(' ', '-')}`}>{e.name}</Link></li>)}</ul>
+                }
+                <span className='bold'>Production companies</span>
+                {movie.production_companies.length === 1 ?
+                    <p><Link className='customLink' to={`/companie/${movie.production_companies[0].id}/${movie.production_companies[0].name.toLowerCase().replaceAll(' ', '-')}`}>{movie.production_companies[0].name}</Link></p>
+                    :
+                    <ul>{movie.production_companies.map(e => <li><Link className='customLink' to={`/companie/${e.id}/${e.name.toLowerCase().replaceAll(' ', '-')}`}>{e.name}</Link></li>)}</ul>}
+                {movie.vote_average && movie.vote_count ?
+                    <>
+                        <span className='bold'>Vote Count</span>
+                        <p>{movie.vote_count} votes</p>
+                        <span className='bold'>Vote Average</span>
+                        <p>{movie.vote_average} of 10</p>
+                    </>
+                    : null
+                }
+            </div>
         </div>
     );
 }
