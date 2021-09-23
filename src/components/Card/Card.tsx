a87005133f4caad07f545ea4a58f8a6f9d31ab65
EdwardBurgos <edwardpbn@gmail.com>
2021-09-23 07:28:31 -0500

    improved date format | toastify color modified

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,11 +4,13 @@ import defaultPoster from '../../img/defaultPoster.jpg';
 import { Link } from 'react-router-dom';
 
 export default function Card({ movie }: CardProps) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
+
     return (
         <div className={s.card}>
         <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : defaultPoster} alt={movie.title} className={s.poster}></img>
         <Link className='bold mb-0 customLink' to={`/detail/${movie.id}`}>{movie.title}</Link>
-        { movie.release_date ? <p className='mb-0'>{movie.release_date}</p> : null}
+        { movie.release_date ? <p className='mb-0'>{`${movie.release_date.split('-')[2]} ${months[parseInt(movie.release_date.split('-')[1])]} ${movie.release_date.split('-')[0]}`}</p> : null}
         
         </div>
     );
