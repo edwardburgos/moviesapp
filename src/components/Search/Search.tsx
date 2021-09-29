6cb756a973c40860d65cbf843dd22e554c66bff7
EdwardBurgos <edwardpbn@gmail.com>
2021-09-29 15:35:08 -0500

    sorting implemented in search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -32,6 +32,8 @@ export default function SearchBar() {
         { name: 'Collections', nameSingular: 'collection', search: 'Search a collection', value: '4' },
         { name: 'Genres', nameSingular: 'genre', value: '5' }
     ];
+    const [sorting, setSorting] = useState('popularity.desc')
+
 
     const dispatch = useDispatch();
 
@@ -78,6 +80,12 @@ export default function SearchBar() {
         }
     }
 
+    async function sortBy(sortParameter: string) {
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=${sortParameter}&page=1&with_genres=${genre}`
+        const results = await axios.get(url)
+        dispatch(modifyResults(results.data.results))
+    }
+
     return (
         <div className='flexCentered'>
             <div className={`${s.test} ${results ? 'mb-3' : ''}`}>
@@ -105,15 +113,38 @@ export default function SearchBar() {
                             <img src={title ? closeCircle : search} className={s.iconDumb} onClick={() => { setTitle(''); dispatch(modifyResults(null)); }} alt={title ? 'Remove movie title' : 'Search a movie'} />
                         </div>
                         :
-                        <div className={s.selectContainer}>
-                            <Form.Select aria-label="Default select example" value={genre} onChange={(e) => { const target = e.target as HTMLSelectElement; searchByGenre(target.value); setGenre(target.value) }}>
-                                {genre ? null : <option>Select a movie genre</option>}
-                                {genres.map((e, index) => <option value={e.id} key={index}>{e.name}</option>)}
-                            </Form.Select>
-                            <div className={genre === '' ? s.invisible : s.iconContainer}>
-                                <img src={closeCircle} className={genre === '' ? s.invisible : s.selectIconDumb} onClick={() => { dispatch(modifyResults(null)); setGenre(''); }} alt={'Remove selected genre'} />
+                        <>
+                            <div className={s.selectContainer}>
+                                <Form.Select aria-label="Default select example" value={genre} onChange={(e) => { const target = e.target as HTMLSelectElement; searchByGenre(target.value); setGenre(target.value) }}>
+                                    {genre ? null : <option>Select a movie genre</option>}
+                                    {genres.map((e, index) => <option value={e.id} key={index}>{e.name}</option>)}
+                                </Form.Select>
+                                <div className={genre === '' ? s.invisible : s.iconContainer}>
+                                    <img src={closeCircle} className={genre === '' ? s.invisible : s.selectIconDumb} onClick={() => { dispatch(modifyResults(null)); setGenre(''); }} alt={'Remove selected genre'} />
+                                </div>
                             </div>
-                        </div>
+                            {
+                                results && results.length ?
+                                    <div className={s.selectContainer}>
+                                        <Form.Select className={s.selectInput} aria-label="Default select example" value={sorting} onChange={(e) => { const target = e.target as HTMLSelectElement; sortBy(target.value); setSorting(target.value) }}>
+                                            <option value='popularity.asc'>Sort by current popularity ascending</option>
+                                            <option value='popularity.desc'>Sort by current popularity descending</option>
+                                            <option value='primary_release_date.asc'>Sort by release date ascending</option>
+                                            <option value='primary_release_date.desc'>Sort by release date descending</option>
+                                            <option value='revenue.asc'>Sort by revenue ascending</option>
+                                            <option value='revenue.desc'>Sort by revenue descending</option>
+                                            <option value='vote_average.asc'>Sort by best rated ascending</option>
+                                            <option value='vote_average.desc'>Sort by best rated descending</option>
+                                            <option value='vote_count.asc'>Sort by most rated ascending</option>
+                                            <option value='vote_count.desc'>Sort by most rated descending</option>
+                                        </Form.Select>
+                                        <div className={sorting === 'popularity.desc' ? s.invisible : s.iconContainer}>
+                                            <img src={closeCircle} className={sorting === 'popularity.desc' ? s.invisible : s.iconDumb} onClick={() => { sortBy('popularity.desc'); setSorting('popularity.desc'); }} alt={'Remove selected sorting'} />
+                                        </div>
+                                    </div>
+                                    : null
+                            }
+                        </>
                 }
             </div>
             {results ?
